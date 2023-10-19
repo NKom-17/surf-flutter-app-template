6b84598c2354e108ca9c76d3536506fa56df0d41
Nikolay Komarov <komarov6673@gmail.com>
2023-10-19 15:44:45 +0400

    fix: correction of comments part three * fix: add a new text style for the text on the card
    * fix: add a new property for the shadow color

--- lib/features/photos/widgets/photos_grid.dart
+++ lib/features/photos/widgets/photos_grid.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_template/assets/colors/color_scheme.dart';
+import 'package:flutter_template/assets/text/text_extention.dart';
 import 'package:flutter_template/features/photos/screens/photos_screen/photos_screen.dart';
 import 'package:flutter_template/l10n/app_localizations_x.dart';
 
 /// [PhotosScreen] content
-
 class PhotosGrid extends StatelessWidget {
   /// Create an instance [PhotosGrid].
   const PhotosGrid({super.key});
@@ -16,8 +16,7 @@ class PhotosGrid extends StatelessWidget {
       sliver: SliverGrid.builder(
         itemCount: 10,
         itemBuilder: (_, index) {
-          final shadowColor = AppColorScheme.of(context).onBackground;
-          return _PhotoCard(shadowColor);
+          return const _PhotoCard();
         },
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
@@ -30,18 +29,17 @@ class PhotosGrid extends StatelessWidget {
 }
 
 class _PhotoCard extends StatelessWidget {
-  const _PhotoCard(this.shadowColor);
-
-  final Color shadowColor;
+  const _PhotoCard();
 
   @override
   Widget build(BuildContext context) {
     const numberOfLikes = 14;
     const username = 'Christian';
+    final cardShadow = AppColorScheme.of(context).cardShadow;
 
     return Card(
       elevation: 8,
-      shadowColor: shadowColor,
+      shadowColor: cardShadow,
       clipBehavior: Clip.antiAlias,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(20),
@@ -83,12 +81,15 @@ class _TextInfoOnCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final scheme = AppColorScheme.of(context);
+    final textTheme = AppTextTheme.of(context);
 
     return Text(
       text,
       style: TextStyle(
-        fontSize: 12,
-        fontWeight: isUsernameText ? FontWeight.w600 : null,
+        fontSize: textTheme.bold12.fontSize,
+        fontWeight: isUsernameText
+            ? textTheme.bold12.fontWeight
+            : textTheme.regular12.fontWeight,
         color: scheme.onPrimary,
       ),
     );
